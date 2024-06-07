503a7829a335beaba15b62acca03f5d5b412e8fb
odd:odd <eehd80yul@gmail.com>
2024-06-07 17:07:07 +0900

    webD js

--- 04-css/js/comm.js
+++ 04-css/js/comm.js
@@ -18,19 +18,50 @@ $(function () {
     // 'header' 클릭시
     // $('header').click(function () {
     //     // 'header'에게 'fixed' 클래스 추가
-    //     $('header').addClass('fixed')
+    //     $(this).addClass('fixed')
     // })
 
-    // 스크롤 시 'header'에게 'fixed' 클래스 추가
+    // 윈도우 스크롤 이벤트
     $(window).scroll(function () {
-        // 스크롤 값
-        var scroll = $(window).scrollTop()
-        // console.log(scroll)
-        // 'header'에게 'fixed' 클래스 추가
+        let scroll = $(window).scrollTop()
+        console.log(scroll)
+
+        // 스크롤 값이 100px 이상이면
         if (scroll > 100) {
+            // 조건이 참일 때 실행할 코드
             $('header').addClass('fixed')
         } else {
+            // 조건이 거짓일 때 실행할 코드
             $('header').removeClass('fixed')
         }
+        // 'header'에게 'fixed' 클래스 추가
+        // 그렇지 않으면 'fixed' 클래스 제거
+
+        // $('header').addClass('fixed')
+    })
+
+    // 메뉴 클릭시 해당 섹션으로 이동
+    $(".navbar a, footer a[href='#wrap']").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== '') {
+            // Prevent default anchor click behavior
+            event.preventDefault()
+
+            // Store hash
+            var hash = this.hash
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                900,
+                function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash
+                },
+            )
+        } // End if
     })
 })

acc64d04d48ae4c401808a53aec721f9df984a58
odd:odd <eehd80yul@gmail.com>
2024-06-10 15:01:15 +0900

    slide js

--- 05-js/js/main.js
+++ 05-js/js/main.js
@@ -45,3 +45,66 @@ const post = {
     title: '독보적인 예쁨 인생원피스 45%',
 }
 console.log(post)
+console.log(post.img)
+console.log(post.title)
+
+// 배열(array)
+// 순서가 있는 리스트
+const arr = [1, 2, 3, 4, 5]
+console.log(arr)
+console.log(arr[0]) // 1
+
+const users = [
+    { name: '홍길동', age: 20 },
+    { name: '김철수', age: 30 },
+    { name: '이영희', age: 40 },
+]
+
+console.log(users[1]) // { name: '김철수', age: 30 }
+console.log(`${users[1].name} 님 안녕하세요.`) // 김철수
+
+// 함수
+// 기명 함수
+function 함수이름() {}
+함수이름()
+
+// 익명 함수
+const fun1 = function () {
+    console.log('fun1')
+}
+fun1()
+
+// 화살표 함수
+const fun2 = () => {
+    console.log('fun2')
+}
+fun2()
+
+// 조건문
+
+// if문
+const aa = 2
+
+// if (조건) {조건이 참일 때 실행되는 코드}
+if (aa === 1) {
+    // 조건이 참일 때 실행되는 코드
+    console.log('aa는 1입니다.')
+} else if (aa === 2) {
+    console.log('aa는 2입니다.')
+} else {
+    // 조건이 거짓일 때 실행되는 코드
+    console.log('aa는 1이 아닙니다.')
+}
+
+// slide
+let current = 0
+const lstSlide = document.querySelector('.slide ul')
+
+// 3초마다 슬라이드가 넘어가도록
+// setInterval(함수, 시간)
+setInterval(function () {
+    slide() // 슬라이드 함수 호출
+}, 3000)
+
+// 슬라이드 함수 정의
+function slide() {}

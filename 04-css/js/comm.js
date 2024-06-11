26a03c38135bd1bc9684abdb873e8eb513d414e1
odd:odd <eehd80yul@gmail.com>
2024-06-11 11:39:19 +0900

    aos

--- 04-css/js/comm.js
+++ 04-css/js/comm.js
@@ -6,14 +6,14 @@ $(function () {
     // #logo 클릭시 액션 발생
     $('#logo').click(function () {
         // action 값
-        $('#layerPop').fadeIn(5000)
-    })
+        $('#layerPop').fadeIn(5000);
+    });
 
     // '#layerPop button' 클릭시
     $('#layerPop button').click(function () {
         // action 값
-        $('#layerPop').fadeOut(3000)
-    })
+        $('#layerPop').fadeOut(3000);
+    });
 
     // 'header' 클릭시
     // $('header').click(function () {
@@ -23,32 +23,32 @@ $(function () {
 
     // 윈도우 스크롤 이벤트
     $(window).scroll(function () {
-        let scroll = $(window).scrollTop()
-        console.log(scroll)
+        let scroll = $(window).scrollTop();
+        console.log(scroll);
 
         // 스크롤 값이 100px 이상이면
         if (scroll > 100) {
             // 조건이 참일 때 실행할 코드
-            $('header').addClass('fixed')
+            $('header').addClass('fixed');
         } else {
             // 조건이 거짓일 때 실행할 코드
-            $('header').removeClass('fixed')
+            $('header').removeClass('fixed');
         }
         // 'header'에게 'fixed' 클래스 추가
         // 그렇지 않으면 'fixed' 클래스 제거
 
         // $('header').addClass('fixed')
-    })
+    });
 
     // 메뉴 클릭시 해당 섹션으로 이동
     $(".navbar a, footer a[href='#wrap']").on('click', function (event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== '') {
             // Prevent default anchor click behavior
-            event.preventDefault()
+            event.preventDefault();
 
             // Store hash
-            var hash = this.hash
+            var hash = this.hash;
 
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
@@ -59,9 +59,21 @@ $(function () {
                 900,
                 function () {
                     // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash
+                    window.location.hash = hash;
                 },
-            )
+            );
         } // End if
-    })
-})
+    });
+
+    // aos
+    AOS.init({
+        // key: value,
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 600, // values from 0 to 3000, with step 50ms
+        easing: 'ease-in-out', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-center',
+    });
+});
--- 05-js/js/main.js
+++ 05-js/js/main.js
@@ -1,111 +1,111 @@
 // js구문
 // 함수란 특정 작업을 수행하는 코드 블록
 function myF() {
-    const demo = document.querySelector('.demo')
+    const demo = document.querySelector('.demo');
 
     // document.getElementById('demo').innerHTML = 'Hello javascript!!'
-    demo.innerHTML = 'Hello javascript!!'
-    demo.style.color = 'red'
-    demo.style.fontSize = '30px'
-    demo.style.backgroundColor = 'yellow'
-    demo.style.transform = 'translate(100px, 210px) rotate(45deg) scale(1.5)'
+    demo.innerHTML = 'Hello javascript!!';
+    demo.style.color = 'red';
+    demo.style.fontSize = '30px';
+    demo.style.backgroundColor = 'yellow';
+    demo.style.transform = 'translate(100px, 210px) rotate(45deg) scale(1.5)';
 }
 
 // 변수
 // 변수란 데이터를 저장하는 공간
 // 변수 선언 (let, const, var)
 
-let a = 20
-a = 10
-console.log(a)
+let a = 20;
+a = 10;
+console.log(a);
 
-const b = 30
+const b = 30;
 // b = 40
-console.log(b)
+console.log(b);
 
 // 데이터 타입
 // 숫자(number)
-let num1 = 1
-let num2 = 2
-console.log(num1 + num2) // 3
+let num1 = 1;
+let num2 = 2;
+console.log(num1 + num2); // 3
 
 // 문자열(string)
-let str1 = '1d'
-let str2 = '2d'
-console.log(str1 + str2) // 12
+let str1 = '1d';
+let str2 = '2d';
+console.log(str1 + str2); // 12
 
 // 불린(boolean)
-const isTrue = true
-const isFalse = false
-console.log(isTrue, isFalse)
+const isTrue = true;
+const isFalse = false;
+console.log(isTrue, isFalse);
 
 // 객체(object)
 const post = {
     img: 'https://s.pstatic.net/shopping.phinf/20240402_6/60af39c7-fad0-42a8-b672-55813cfcbf04.jpg?type=f294_378',
     title: '독보적인 예쁨 인생원피스 45%',
-}
-console.log(post)
-console.log(post.img)
-console.log(post.title)
+};
+console.log(post);
+console.log(post.img);
+console.log(post.title);
 
 // 배열(array)
 // 순서가 있는 리스트
-const arr = [1, 2, 3, 4, 5]
-console.log(arr)
-console.log(arr[0]) // 1
+const arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr[0]); // 1
 
 const users = [
     { name: '홍길동', age: 20 },
     { name: '김철수', age: 30 },
     { name: '이영희', age: 40 },
-]
+];
 
-console.log(users[1]) // { name: '김철수', age: 30 }
-console.log(`${users[1].name} 님 안녕하세요.`) // 김철수
+console.log(users[1]); // { name: '김철수', age: 30 }
+console.log(`${users[1].name} 님 안녕하세요.`); // 김철수
 
 // 함수
 // 기명 함수
 function 함수이름() {}
-함수이름()
+함수이름();
 
 // 익명 함수
 const fun1 = function () {
-    console.log('fun1')
-}
-fun1()
+    console.log('fun1');
+};
+fun1();
 
 // 화살표 함수
 const fun2 = () => {
-    console.log('fun2')
-}
-fun2()
+    console.log('fun2');
+};
+fun2();
 
 // 조건문
 
 // if문
-const aa = 2
+const aa = 2;
 
 // if (조건) {조건이 참일 때 실행되는 코드}
 if (aa === 1) {
     // 조건이 참일 때 실행되는 코드
-    console.log('aa는 1입니다.')
+    console.log('aa는 1입니다.');
 } else if (aa === 2) {
-    console.log('aa는 2입니다.')
+    console.log('aa는 2입니다.');
 } else {
     // 조건이 거짓일 때 실행되는 코드
-    console.log('aa는 1이 아닙니다.')
+    console.log('aa는 1이 아닙니다.');
 }
 
 // slide
-let current = 0
-const lstSlide = document.querySelector('.slide ul')
-const slideCount = document.querySelectorAll('.slide li').length
+let current = 0;
+const lstSlide = document.querySelector('.slide ul');
+const slideCount = document.querySelectorAll('.slide li').length;
 
 // 3초마다 슬라이드가 넘어가도록
 // setInterval(함수, 시간)
 setInterval(function () {
-    slide() // 슬라이드 함수 호출
-}, 3000)
+    slide(); // 슬라이드 함수 호출
+}, 3000);
 
 // 슬라이드 함수 정의
 function slide() {
@@ -113,6 +113,6 @@ function slide() {
     // 0 * -100% = 0 (left: 0)
     // 1 * -100% = -100% (left: -100%)
     // 2 * -100% = -200% (left: -200%)
-    current = (current + 1) % slideCount
-    lstSlide.style.left = current * -100 + '%'
+    current = (current + 1) % slideCount;
+    lstSlide.style.left = current * -100 + '%';
 }
